1afd4c0e83f1822e7df9ded4aefdbe17a2fdf53b
Nathan Reed <reednj@gmail.com>
2015-01-17 11:48:11 +0800

    refacor give display to be simpler, and just operate on the oldest process

--- i2c/utils/give-display.rb
+++ i2c/utils/give-display.rb
@@ -13,7 +13,7 @@ def main
 	end
 
 	search_for = ARGV[0]
-	pid = get_pids(search_for).first
+	pid = get_pid(search_for)
 
 	if pid.nil? || pid == 0 || pid == Process.pid
 		puts "no pid found for '#{search_for}'"
@@ -23,15 +23,9 @@ def main
 	PIDLock.give_lock pid
 end
 
-def get_pids search_for
-	pids = `pgrep -f "#{search_for}"`
-	pids.split("\n").map do |p| 
-		if p.to_i == Process.pid || p.to_i == Process.ppid
-			nil
-		else
-			p.to_i
-		end
-	end
+def get_pid(search_for)
+	pids = `pgrep -o -f "#{search_for}"`
+	pids.strip
 end
 
 main()
