58ec6f77c0101f9bc476e8767e3feafe25d5bb46
Nathan Reed <reednj@gmail.com>
2015-10-04 16:02:33 +0800

    save the uploaded pics, instead of just overwriting everytime

--- camera/webcam.rb
+++ camera/webcam.rb
@@ -6,7 +6,9 @@ require_relative '../i2c/lib/alpha'
 
 def main()
 	display = AlphaDisplayShared.new
-	remote = 'reednj@reednj.com:~/reednj.com/rpi.jpg'
+	
+	server = 'reednj@reednj.com'
+	remote = "#{server}:~/reednj.com/#{remote_path}"
 	ph = PhotoHelper.new '~/photos', :simple => true
 	
 	display.set 'foto'
@@ -15,11 +17,16 @@ def main()
 	
 	display.set 'send'
 	`scp #{ph.output_path} #{remote}`
+	`ssh #{server} 'unlink rpi.jpg;ln -s #{remote_path} rpi.jpg'`
 	puts "uploaded to #{remote}"
 
 	display.set 'done'
 	sleep 1.0
 	display.release_display
+end
+
+def remote_path
+	 "pics/#{Time.now.utc.iso8601}.jpg"
 end
 
 main()
