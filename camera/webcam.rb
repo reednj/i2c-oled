61fc25036d0f1a01959343e99834a501347fc58c
Nathan Reed <reednj@gmail.com>
2015-10-04 16:25:33 +0800

    generate the photo path only once

--- camera/webcam.rb
+++ camera/webcam.rb
@@ -4,29 +4,36 @@ require 'time'
 require_relative './rpi-camera'
 require_relative '../i2c/lib/alpha'
 
-def main()
-	display = AlphaDisplayShared.new
-	
-	server = 'reednj@reednj.com'
-	remote = "#{server}:~/reednj.com/#{remote_path}"
-	ph = PhotoHelper.new '~/photos', :simple => true
-	
-	display.set 'foto'
-	ph.capture
-	puts "photo saved at #{ph.output_path}"
-	
-	display.set 'send'
-	`scp #{ph.output_path} #{remote}`
-	`ssh #{server} 'unlink rpi.jpg;ln -s #{remote_path} rpi.jpg'`
-	puts "uploaded to #{remote}"
+class App
+	def initialize
+		@remote_path = nil
+	end
 
-	display.set 'done'
-	sleep 1.0
-	display.release_display
+	def main
+		display = AlphaDisplayShared.new
+		
+		server = 'reednj@reednj.com'
+		remote = "#{server}:#{remote_path}"
+		ph = PhotoHelper.new '~/photos', :simple => true
+		
+		display.set 'foto'
+		ph.capture
+		puts "photo saved at #{ph.output_path}"
+		
+		display.set 'send'
+		`scp #{ph.output_path} #{remote}`
+		`ssh #{server} 'cp #{remote_path} ~/reednj.com/rpi.jpg'`
+		puts "uploaded to #{remote}"
+
+		display.set 'done'
+		sleep 1.0
+		display.release_display
+	end
+
+	def remote_path
+		 @remote_path = "~/rpi/pics/#{Time.now.utc.iso8601}.jpg".gsub(':', '.') if @remote_path.nil?
+		 @remote_path
+	end
 end
 
-def remote_path
-	 "pics/#{Time.now.utc.iso8601}.jpg"
-end
-
-main()
+App.new.main
