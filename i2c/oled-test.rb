0e1393482a723275796b934825b5d7741a518702
Nathan Reed <reednj@gmail.com>
2015-12-27 14:38:31 +0800

    start benchmarking the oled refresh - getting approx 15fps

--- i2c/oled-test.rb
+++ i2c/oled-test.rb
@@ -4,10 +4,14 @@ require './lib/oled'
 def main
 	display = OLEDDisplay.new
 
-	update_loop 0.1 do
-		random_pixel(display)
+	puts "benchmarking oled fps"
+
+	result = benchmark do
+		10.times { random_pixel(display) }
 		display.write_buffer
 	end
+
+	puts "#{result.round(1)} fps"
 
 end
 
@@ -15,7 +19,20 @@ def random_pixel(display)
 	x = (rand * display.width).floor
 	y = (rand * display.height).floor
 	display.set_pixel(x, y, OLEDDisplay::COLOR_WHITE)
-	puts "#{x}, #{y}"
+end
+
+def benchmark(count = 1, &block)
+	start = Time.now
+
+	count.times do |i|
+		yield
+	end
+
+	duration = Time.now - start
+	per_sec = count / duration
+
+	return benchmark(count*2, &block) if duration < 0.5
+	return per_sec.to_f
 end
 
 main
